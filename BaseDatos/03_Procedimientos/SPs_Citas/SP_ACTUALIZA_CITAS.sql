04ef16eba35072936a813cb06b39357e2850c456
EnriqueJSV <48308544+EnriqueJSV@users.noreply.github.com>
2026-08-01 00:30:57 -0600

    Edicion de la validacion SP_ACTUALIZA_CITAS

--- BaseDatos/03_Procedimientos/SPs_Citas/SP_ACTUALIZA_CITAS.sql
+++ BaseDatos/03_Procedimientos/SPs_Citas/SP_ACTUALIZA_CITAS.sql
@@ -3,18 +3,22 @@ GO
 
 CREATE OR ALTER PROCEDURE SP_ACTUALIZA_CITAS
 (
-@Id_Cita INT,
-@Id_Mascota INT,
-@Id_Veterinario INT,
-@Fecha DATE,
-@Hora TIME,
-@Motivo VARCHAR(500),
-@Estado_Cita VARCHAR(50),
+@Id_Cita int,
+@Id_Mascota int,
+@Id_Veterinario int,
+@Fecha date,
+@Hora time,
+@Motivo varchar(500),
+@Estado_Cita varchar(50),
 @IdUsuarioGlobal INT
 )
-AS BEGIN
-	BEGIN TRY
-	BEGIN
+AS BEGIN 
+
+BEGIN TRY
+	IF NOT EXISTS (SELECT @Id_Cita FROM Citas WHERE Id_Mascota=@Id_Mascota and Id_Veterinario=@Id_Veterinario AND Id_Cita<>@Id_Cita) 
+	AND 
+	NOT EXISTS (SELECT Id_Cita FROM Citas WHERE Fecha=@Fecha AND Hora=@Hora AND Id_Cita<>@Id_Cita) -- VALIDA SI EXISTE UNA CITA EN LA MISMA FECHA Y HORA
+	BEGIN 
 		UPDATE Citas
 		SET Id_Mascota=@Id_Mascota, Id_Veterinario=@Id_Veterinario, Fecha=@Fecha, Hora=@Hora, Motivo=@Motivo, Estado_Cita=@Estado_Cita
 		WHERE Id_Cita=@Id_Cita
@@ -24,8 +28,8 @@ AS BEGIN
 		DECLARE @USRNOM VARCHAR(300)
 		DECLARE @ACC CHAR(1)
 
-		SELECT @USRNOM = Nombre_Usuario FROM Usuarios WHERE Id_Usuario=@IdUsuarioGlobal
-		SET @DSC = 'Se actualiza el registro Id: ' + CONVERT(VARCHAR,@Id_Cita) + ' de Citas'
+		SELECT @USRNOM = Nombre_Usuario FROM Usuarios Where Id_Usuario=@IdUsuarioGlobal
+		SET @DSC = 'El Usuario: ' + CONVERT(VARCHAR,@USRNOM) + ' actualiza la información de la cita con ID ' + @Id_Cita
 		SET @ACC = 'A'
 
 		INSERT INTO Auditoria
@@ -33,14 +37,18 @@ AS BEGIN
 		Id_Usuario, Accion, Descripcion, Fecha
 		)
 		SELECT
-		@IdUsuarioGlobal, @ACC, RTRIM(LTRIM(@DSC)), GETDATE()
+		@IdUsuarioGlobal, @ACC, RTRIM(LTRIM(@DSC)) , GETDATE()
 		-----------------------PARA EL CONTROL DE AUDITORIA DEL SISTEMA-------------------------------------------
 
 		SELECT @Id_Cita
 	END
-	END TRY
-	BEGIN CATCH
-		SELECT 0
-	END CATCH
+	ELSE
+	BEGIN 
+		SELECT  -1
+	END
+END TRY
+BEGIN CATCH
+	SELECT 0
+END CATCH
+
 END
-GO
