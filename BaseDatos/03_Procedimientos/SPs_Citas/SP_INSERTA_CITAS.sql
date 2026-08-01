5cd0e3fd48e35587271a47781ce57309e63a5f6e
EnriqueJSV <48308544+EnriqueJSV@users.noreply.github.com>
2026-08-01 00:43:41 -0600

    Refactor SP_INSERTA_CITAS to check for existing records

--- BaseDatos/03_Procedimientos/SPs_Citas/SP_INSERTA_CITAS.sql
+++ BaseDatos/03_Procedimientos/SPs_Citas/SP_INSERTA_CITAS.sql
@@ -13,35 +13,40 @@ CREATE OR ALTER PROCEDURE SP_INSERTA_CITAS
 )
 AS BEGIN
 	BEGIN TRY
-	BEGIN
-		INSERT INTO Citas
-		(
-		[Id_Mascota], [Id_Veterinario], [Fecha], [Hora], [Motivo], [Estado_Cita]
-		)
-		VALUES
-		(
-		@Id_Mascota, @Id_Veterinario, @Fecha, @Hora, @Motivo, @Estado_Cita
-		)
-
-		SELECT @@IDENTITY
-
-		-----------------------PARA EL CONTROL DE AUDITORIA DEL SISTEMA-------------------------------------------
-		DECLARE @DSC VARCHAR(MAX)
-		DECLARE @USRNOM VARCHAR(300)
-		DECLARE @ACC CHAR(1)
-
-		SELECT @USRNOM = Nombre_Usuario FROM Usuarios WHERE Id_Usuario=@IdUsuarioGlobal
-		SET @DSC = 'Se inserta un nuevo registro en Citas (Id: ' + CONVERT(VARCHAR,@@IDENTITY) + ')'
-		SET @ACC = 'I'
-
-		INSERT INTO Auditoria
-		(
-		Id_Usuario, Accion, Descripcion, Fecha
-		)
-		SELECT
-		@IdUsuarioGlobal, @ACC, RTRIM(LTRIM(@DSC)), GETDATE()
-		-----------------------PARA EL CONTROL DE AUDITORIA DEL SISTEMA-------------------------------------------
-	END
+		IF NOT EXISTS (SELECT Id_Cita FROM Citas WHERE Id_Mascota=@Id_Mascota)
+			BEGIN
+				INSERT INTO Citas
+				(
+				[Id_Mascota], [Id_Veterinario], [Fecha], [Hora], [Motivo], [Estado_Cita]
+				)
+				VALUES
+				(
+				@Id_Mascota, @Id_Veterinario, @Fecha, @Hora, @Motivo, @Estado_Cita
+				)
+		
+				SELECT @@IDENTITY
+		
+				-----------------------PARA EL CONTROL DE AUDITORIA DEL SISTEMA-------------------------------------------
+				DECLARE @DSC VARCHAR(MAX)
+				DECLARE @USRNOM VARCHAR(300)
+				DECLARE @ACC CHAR(1)
+		
+				SELECT @USRNOM = Nombre_Usuario FROM Usuarios WHERE Id_Usuario=@IdUsuarioGlobal
+				SET @DSC = 'Se inserta un nuevo registro en Citas (Id: ' + CONVERT(VARCHAR,@@IDENTITY) + ')'
+				SET @ACC = 'I'
+		
+				INSERT INTO Auditoria
+				(
+				Id_Usuario, Accion, Descripcion, Fecha
+				)
+				SELECT
+				@IdUsuarioGlobal, @ACC, RTRIM(LTRIM(@DSC)), GETDATE()
+				-----------------------PARA EL CONTROL DE AUDITORIA DEL SISTEMA-------------------------------------------
+			END
+		ELSE
+			BEGIN
+				SELECT -1
+			END
 	END TRY
 	BEGIN CATCH
 		SELECT 0
