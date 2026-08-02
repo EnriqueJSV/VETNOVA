8a3701ca0622679edcbbcca50948aa5aaac7b5c4
EnriqueJSV <48308544+EnriqueJSV@users.noreply.github.com>
2026-08-01 22:37:47 -0600

    Add Veterinarian details to SP_FILTRAR_CITAS

--- BaseDatos/03_Procedimientos/SPs_Citas/SP_FILTRAR_CITAS.sql
+++ BaseDatos/03_Procedimientos/SPs_Citas/SP_FILTRAR_CITAS.sql
@@ -6,9 +6,10 @@ CREATE OR ALTER PROCEDURE SP_FILTRAR_CITAS
 @Nombre varchar(100)
 )
 AS BEGIN
-	SELECT CIT.Id_Cita, MASC.Nombre AS Mascota, CIT.Fecha, CIT.Hora, CIT.Motivo, CIT.Estado_Cita
-	FROM Mascotas MASC
-	INNER JOIN Citas CIT ON CIT.Id_Mascota=MASC.Id_Mascota
+	SELECT CIT.Id_Cita, MASC.Nombre AS Mascota, VET.Nombre AS Veterinario, CIT.Fecha, CIT.Hora, CIT.Motivo, CIT.Estado_Cita
+	FROM Citas CIT
+	INNER JOIN Mascotas MASC ON MASC.Id_Mascota=CIT.Id_Mascota
+	INNER JOIN Veterinarios VET ON VET.Id_Veterinario=CIT.Id_Veterinario
 	WHERE MASC.Nombre LIKE '%' + @Nombre + '%' 
 	ORDER BY CIT.Fecha, CIT.Hora
 END
