bab3869b24aba0308642d913fd75b52eba6074de
IAN JOSUE QUESADA ORDONEZ <ian.quesada@uamcr.net>
2026-08-02 16:08:41 -0600

    creacion frmMenuPrincipal

---
+++ PL_VETNOVA/Pantallas/Generales/frmMenuPrincipal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PL_VETNOVA.Pantallas.Generales
+{
+    public partial class frmMenuPrincipal : Form
+    {
+        public frmMenuPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
