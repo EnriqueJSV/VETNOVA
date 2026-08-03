b4b28e6b82e3001c612c07b78e987dcda3fefc69
IAN JOSUE QUESADA ORDONEZ <ian.quesada@uamcr.net>
2026-08-02 18:12:01 -0600

    se crea y dissena frmInicioSesion

--- PL_VETNOVA/Program.cs
+++ PL_VETNOVA/Program.cs
@@ -16,7 +16,7 @@ namespace PL_VETNOVA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Pantallas.Generales.frmInicioSesion());
         }
     }
 }
